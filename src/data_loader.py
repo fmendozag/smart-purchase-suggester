24069853241aed7237a5c7160626321710926350
Freddy Mendoza <geovanny.mendoza94@gmail.com>
2025-09-10 05:56:34 -0500

    Fix and test load_sales function in the notebook

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,16 +1,29 @@
 import pandas as pd
 
 def load_sales(path:str) -> pd.DataFrame:
-    df = pd.read_csv(path, header=None, names=['sale_date','product_id','quantity','total_amount'])
-    df['sale_date'] = pd.to_datetime(df['sale_date'])
+    df = pd.read_csv(path, 
+                     sep=';',
+                     header=None, 
+                     names=['sale_date','product_id','quantity','total_amount'],
+                     dtype=str)
+    df['product_id'] = df['product_id'].astype(str).str.zfill(10)
+    df['sale_date'] = pd.to_datetime(df['sale_date'], format="%d/%m/%Y", errors="coerce")
+    df['quantity'] = df['quantity'].str.replace(",", ".", regex=False).astype(float)
+    df['total_amount'] = df['total_amount'].str.replace(",", ".", regex=False).astype(float)
     return df.dropna()
 
 def load_purchases(path:str) -> pd.DataFrame:
-    df = pd.read_csv(path, header=None, names=['purchase_date','product_id','supplier_name','quantity_purchased','total_amount'])
-    df['purchase_date'] = pd.to_datetime(df['purchase_date'])
+    df = pd.read_csv(path, 
+                     sep=';',
+                     header=None, 
+                     names=['purchase_date','product_id','supplier_name','quantity_purchased','total_amount'])
+    df['purchase_date'] = pd.to_datetime(df['purchase_date'],dayfirst=True, errors='coerce')
     return df
 
 def load_products(path:str) -> pd.DataFrame:
-    df = pd.read_csv(path,header=None, names=['product_id','product_code','product_name','current_stock','unit_conversion'])
+    df = pd.read_csv(path,
+                     sep=';',
+                     header=None, 
+                     names=['product_id','product_code','product_name','current_stock','unit_conversion'])
     return df
 
