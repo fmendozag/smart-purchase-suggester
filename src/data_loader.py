60057b14198365c5cccdb3055f03e59ff31b35eb
fmendozag <geovanny.mendoza94@gmail.com>
2025-09-11 20:07:00 -0500

    Performed testing and made function modifications in the notebook

--- src/data_loader.py
+++ src/data_loader.py
@@ -16,8 +16,13 @@ def load_purchases(path:str) -> pd.DataFrame:
     df = pd.read_csv(path, 
                      sep=';',
                      header=None, 
-                     names=['purchase_date','product_id','supplier_name','quantity_purchased','total_amount'])
+                     names=['purchase_date','product_id','supplier_name','quantity_purchased','total_amount'],
+                     dtype=str)
     df['purchase_date'] = pd.to_datetime(df['purchase_date'],dayfirst=True, errors='coerce')
+    df['product_id'] = df['product_id'].astype(str).str.zfill(10)
+    df['quantity_purchased'] = df['quantity_purchased'].str.replace(",", ".", regex=False).astype(float)
+    df['total_amount'] = df['total_amount'].str.replace(",", ".", regex=False).astype(float)
+
     return df
 
 def load_products(path:str) -> pd.DataFrame:
@@ -25,5 +30,6 @@ def load_products(path:str) -> pd.DataFrame:
                      sep=';',
                      header=None, 
                      names=['product_id','product_code','product_name','current_stock','unit_conversion'])
+    df['product_id'] = df['product_id'].astype(str).str.zfill(10)    
     return df
 
--- src/stock_manager.py
+++ src/stock_manager.py
@@ -1,11 +1,5 @@
 import pandas as pd
 
-def load_stock(path: str) -> pd.DataFrame:
-    """Carga stock desde CSV/SQL y devuelve ['product_id','current_stock']"""
-    df = pd.read_csv(path, sep=";")
-    df = df.rename(columns={"producto_id":"product_id","stock_actual":"current_stock"})
-    return df[["product_id","current_stock"]]
-
 def calculate_min_stock(sales: pd.DataFrame, days: int = 3) -> pd.DataFrame:
     """Calcula stock mínimo dinámico para cubrir X días de demanda."""
     sales["sale_date"] = pd.to_datetime(sales["sale_date"])
